e56c4a088a0a3f01fdd17cbc939f219454640b84
oligarc <oliver25399@gmail.com>
2025-08-07 13:23:50 +0200

    feat:first_security_config

--- backend/src/main/java/com/oligarc/online_shop/config/CustomUserDetailsService.java
+++ backend/src/main/java/com/oligarc/online_shop/config/CustomUserDetailsService.java
@@ -1,4 +1,31 @@
 package com.oligarc.online_shop.config;
 
-public class CustomUserDetailsService {
+import com.oligarc.online_shop.model.User;
+import com.oligarc.online_shop.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    //UserDetailsService is a Spring Security interface that defines how to load an user from the db
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository v_userRepository){
+        this.userRepository=v_userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found with email: " +email));
+        return org.springframework.security.core.userdetails.User.
+                withUsername(user.getEmail())
+                .password("{noop}" + user.getPassword())
+                .roles("USER")
+                .build();
+    }
 }
--- backend/src/main/java/com/oligarc/online_shop/config/SecurityConfig.java
+++ backend/src/main/java/com/oligarc/online_shop/config/SecurityConfig.java
@@ -1,4 +1,37 @@
 package com.oligarc.online_shop.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable()) //
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/test/user/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .userDetailsService(customUserDetailsService)
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
 }
+
